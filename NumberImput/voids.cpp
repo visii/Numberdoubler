5cc33b050f5e2b858f64f6b5567f01ad01547fd5
Visii <visiiiix@gmail.com>
2018-11-04 19:54:06 -0200

    Best practise changes

--- NumberImput/voids.cpp
+++ NumberImput/voids.cpp
@@ -5,14 +5,14 @@
 
 void printStuff()
 {
-	int64_t x = userNumber();
+	int64_t x{ userNumber() };
 	std::cout << x << " doubled is:" << doubleNumber(x) << std::endl;
 	printStuff1();
 }
 
 void printStuff1()
 {
-	int64_t x = userNumber();
+	int64_t x{ userNumber() };
 	std::cout << x << " doubled is:" << doubleNumber(x) << std::endl;
 	printStuff();
 }
